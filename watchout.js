eed4717990d6a2b18524f0f3266a6a7f3b2b79ec
Siddharth Sukumar <siddharth@inauth.com>
2015-04-04 12:05:31 -0700

    Finished implementing collision detection

--- watchout.js
+++ watchout.js
@@ -1,56 +1,99 @@
 // start slingin' some d3 here.
 
-//FOR DRAGGING AN ELEMENT
-// var selectedElement = 0;
-// var currentX = 0;
-// var currentY = 0;
-// var currentMatrix = 0;
 
-//   function selectElement(evt) {
 
-//     selectedElement = evt.target;
-//     currentX = evt.clientX;
-//     currentY = evt.clientY;
-//     currentMatrix = selectedElement.getAttributeNS(null, "transform").slice(7,-1).split(' ');
+var generateElements = function (n) {
+	var object = [];
 
-//       for(var i=0; i<currentMatrix.length; i++) {
+   for (var i = 0; i < n; i++) {  
+   	 var element = {};
+   	  element.x = Math.floor(Math.random() * 500) + 0;
+      element.y = Math.floor(Math.random()* 800) + 0;
+      element.r = 10;
+      object.push (element);
 
-//         currentMatrix[i] = parseFloat(currentMatrix[i]);
+   } 
+   return object;
+}
 
-//       }
+  var elements = generateElements (50);
 
-//         selectedElement.setAttributeNS(null, "onmousemove", "moveElement(evt)");
-//         selectedElement.setAttributeNS(null, "onmouseout", "deselectElement(evt)"); 
-//        selectedElement.setAttributeNS(null, "onmouseup", "deselectElement(evt)");
 
-//   }
+  var boxWidth = 800;
+  var boxHeight = 500;
 
-//   function moveElement(evt){
+  var box = d3.select('body')
+              .append('svg')
+              .attr('class', 'box')
+              .attr('width', boxWidth)
+              .attr('height', boxHeight);
 
-//     dx = evt.clientX - currentX;
-//     dy = evt.clientY - currentY;
-//     currentMatrix[4] += dx;
-//     currentMatrix[5] += dy;
-//     newMatrix = "matrix(" + currentMatrix.join(' ') + ")";
+  var drag = d3.behavior.drag()
+             .on('dragstart', function() { circle.style('fill', 'red'); })
+             .on('drag', function() { circle.attr('cx', d3.event.x)
+                                            .attr('cy', d3.event.y); })
+             .on('dragend', function() { circle.style('fill', 'black'); });
+      debugger;
 
-//     selectedElement.setAttributeNS(null, "transform", newMatrix);
-//     currentX = evt.clientX;
-//     currentY = evt.clientY;
+  var player = box.selectAll('.draggableCircle')
+                .data([{ x: (boxWidth / 2), y: (boxHeight / 2), r: 13 }])
+                .enter()
+                .append('svg:circle')
+                .attr('class', 'draggableCircle')
+                .attr('cx', function(d) { return d.x; })
+                .attr('cy', function(d) { return d.y; })
+                .attr('r', function(d) { return d.r; })
+                .call(drag)
+                .style('fill', 'black');
 
-// }
+  var enemies = box.selectAll('.enemies')
+                .data(elements)
+                .enter()
+                .append('svg:circle')
+                .attr('class', 'enemies')
+                .attr('cx', function(d) { return d.x; })
+                .attr('cy', function(d) { return d.y; })
+                .attr('r', function(d) { return d.r; })
+                .style ("fill", "green")
+                .style ({
+                   top: 50,
+                   left:69
+                });
 
-// function deselectElement(evt){
+  var checkCollision = function(enemies, callback) {
+  for (var i = 0; i < enemies[0].length; i++) {
+    var radiusSum = parseFloat(enemies[0][i]["attributes"]["r"]["nodeValue"]) + parseFloat(player[0][0]["attributes"]["r"]["nodeValue"]);
+    var xDiff = parseFloat(enemies[0][i]["attributes"]["cx"]["nodeValue"]) - parseFloat(player[0][0]["attributes"]["cx"]["nodeValue"]);
+    var yDiff = parseFloat(enemies[0][i]["attributes"]["cy"]["nodeValue"]) - parseFloat(player[0][0]["attributes"]["cy"]["nodeValue"]);
+    var separation = Math.sqrt(Math.pow(xDiff, 2) + Math.pow(yDiff, 2));
+    if (separation < radiusSum) {
+      return callback();
+    }
+  }
+};
 
-//   if(selectedElement != 0){
 
-//     selectedElement.removeAttributeNS(null, "onmousemove");
-//     selectedElement.removeAttributeNS(null, "onmouseout");
-//     selectedElement.removeAttributeNS(null, "onmouseup");
-//     selectedElement = 0;
+  var move = function (){
+    debugger;
+  enemies.transition().duration (1000)
+     .attr('cx', function(d) { return Math.floor(Math.random() * (200 + 1)) + 0 + d.x; })
+     .attr("cy", function(d) { return Math.floor(Math.random() * (200 + 1)) + 0 + d.y; }) ;
 
-//   }
+  checkCollision(enemies , function (){
+    console.log(1);
+   });    
+   }; 
+   
+  
+setInterval(move, 1000);
 
-// }
+
+
+
+
+  
+
+
 
 
 
